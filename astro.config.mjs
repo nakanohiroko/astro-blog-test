b77c9337d9d0ef60bc709de15d9a754145e42f60
nakanohiroko <piroconbu@gmail.com>
2025-02-06 21:15:45 +0900

    SSRを有効に

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
+import vercel from "@astrojs/vercel/server"; // VercelでSSRする場合 (適宜変更)
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+	output: "server", // SSRを有効化
   adapter: vercel(),
 });
