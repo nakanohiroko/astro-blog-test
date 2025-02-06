d3cd9e079fc6dbefa0e06327b4f903edf334504c
nakanohiroko <piroconbu@gmail.com>
2025-02-06 22:24:38 +0900

    最初の状態に戻す

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,5 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel";
-import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [react()],
-	output: "server",
-  adapter: vercel(),
-});
+export default defineConfig({});
--- src/libs/microcms.ts
+++ src/libs/microcms.ts
@@ -1,42 +1,40 @@
 // SDK利用準備
 import type {
-    MicroCMSQueries,
-    MicroCMSListContent,
-    MicroCMSImage,
-  } from 'microcms-js-sdk';
-  import { createClient } from 'microcms-js-sdk';
-  
-  const client = createClient({
-    serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
-    apiKey: import.meta.env.MICROCMS_API_KEY,
+  MicroCMSQueries,
+  MicroCMSListContent,
+  MicroCMSImage,
+} from 'microcms-js-sdk';
+import { createClient } from 'microcms-js-sdk';
+
+const client = createClient({
+  serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: import.meta.env.MICROCMS_API_KEY,
+});
+
+// 型定義
+export type Tag = {
+  name: string;
+} & MicroCMSListContent;
+
+export type Blog = {
+  title: string;
+  body: string;
+  thumbnail: MicroCMSImage;
+  tags: Tag[];
+} & MicroCMSListContent;
+
+// APIの呼び出し
+export const getBlogList = async (queries?: MicroCMSQueries) => {
+  return await client.getList<Blog>({ endpoint: 'blog', queries });
+};
+
+export const getBlogDetail = async (
+  contentId: string,
+  queries?: MicroCMSQueries
+) => {
+  return await client.getListDetail<Blog>({
+    endpoint: 'blog',
+    contentId,
+    queries,
   });
-  
-  // 型定義
-  export type Tag = {
-    name: string;
-  } & MicroCMSListContent;
-  
-  export type Blog = {
-    title: string;
-    body: string;
-    thumbnail: MicroCMSImage;
-    tags: Tag[];
-  } & MicroCMSListContent;
-  
-  
-  // APIの呼び出し
-  export const getBlogList = async (queries?: MicroCMSQueries) => {
-    return await client.getList<Blog>({ endpoint: 'blog', queries });
-  };
-  
-  export const getBlogDetail = async (
-    contentId: string,
-    queries?: MicroCMSQueries
-  ) => {
-    return await client.getListDetail<Blog>({
-      endpoint: 'blog',
-      contentId,
-      queries,
-    });
-  };
-  
+};
