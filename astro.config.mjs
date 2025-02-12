e28855a2430e9130079186b5538331f939bad6b6
nakanohiroko <piroconbu@gmail.com>
2025-02-12 16:51:57 +0900

    Draft Modeを実装

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,12 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'server',
-	adapter: vercel(),
+  output: 'server',
+  adapter: vercel({
+    isr: {
+      expiration: 60,
+      bypassToken: 'f3a13dad-b3d3-635a-94b7-eed969401a99',
+      exclude: ['/draft'],
+    },
+  }),
 });
