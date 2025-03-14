1f2c33b7da52a61a4aa374a91c06af37833709d2
nakanohiroko <piroconbu@gmail.com>
2025-03-14 18:57:25 +0900

    excludeに/[blogId]も追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,11 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel({
+    isr: {
+      expiration: 60,
+      bypassToken: 'f3a13dad-b3d3-635a-94b7-eed969401a99',
+      exclude: ['/draft','/[blogId]'],
+    },
+  }),
 });
