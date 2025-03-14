8bf70f7cf3b88c0275dd7bf520d136480b29d2cd
nakanohiroko <piroconbu@gmail.com>
2025-03-14 18:42:09 +0900

    astro.config.mjsからISRに関する設定を一旦削除

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,5 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel({
-    isr: {
-      expiration: 60,
-      bypassToken: 'f3a13dad-b3d3-635a-94b7-eed969401a99',
-      exclude: ['/draft'],
-    },
-  }),
+  adapter: vercel(),
 });
